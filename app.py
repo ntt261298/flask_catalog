4386e7155ad69b09570c80af0d74a7ca169de157
Truong Nguyen <ntt261298@gmail.com>
2021-09-11 13:37:12 +0700

    feat: Initial migration

--- app.py
+++ app.py
@@ -8,10 +8,16 @@ from config import app_config
 
 app = Flask(__name__)
 
-if os.environ['ENV'] in ['local', 'development', 'production']:
+if os.environ.get('ENV') in ['local', 'development', 'production']:
     app.config.from_object(app_config[os.environ['ENV']])
 else:
     app.config.from_object(app_config['local'])
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+from models.user import User
+from models.admin import Admin
+from models.category import Category
+from models.item import Item
+
